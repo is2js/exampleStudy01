2a86cdbacfc61514182ff0b6caf4ae67b861836c
is2js <tingstyle1@gmail.com>
2021-11-24 12:18:18 +0900

    요구사항5: 검색기능(제목) 개발 + 조회(for문출력)함수 파라미터 리팩토링  + Test

--- src/main/java/board/Board.java
+++ src/main/java/board/Board.java
@@ -3,16 +3,10 @@ package board;
 import java.util.ArrayList;
 import java.util.Scanner;
 
-// 1.데이터가 2개이상 표현되는 단위는 무조건 묶은 class로 생각한다
-// -> Article class를 만든다.
 public class Board {
 
     Scanner scanner = new Scanner(System.in);
-    /// 5. 저장을 3개 list에 각각 -> 1개 list에 Class타입의 객체를 보관
     ArrayList<Article> articles = new ArrayList<>();
-    //    ArrayList<String> titles = new ArrayList<>();
-    //    ArrayList<String> bodies = new ArrayList<>();
-    //    ArrayList<Integer> numbers = new ArrayList<>();
     int no = 1;
 
     public void runBoard() {
@@ -27,113 +21,131 @@ public class Board {
             }
 
             if (command.equals("help")) {
-                //13. 구조화 완료후  help에 기능 추가해주기
-                System.out.println("add  : 게시물 등록");
-                System.out.println("list : 게시물 목록 조회");
-                System.out.println("update : 게시물 수정");
-                System.out.println("delete : 게시물 삭제");
-                //14. 앞으로 만들 검색
-                System.out.println("search : 게시물 검색");
-
+                printHelp();
                 continue;
             }
             if (command.equals("add")) {
-                System.out.print("제목을 입력해주세요 : ");
-                String title = scanner.nextLine();
-                System.out.print("내용을 입력해주세요 : ");
-                String body = scanner.nextLine();
-                // 2. 데이터보관용 class가 만들어졌다면, 인스턴스를 1개 만든 뒤,
-                // add: 기존 개별 데이터들을 한꺼번에 담는다.
-                // -> 아직 private 변수 -> 생성자 초기화 -> getter, setter로 접근 미완성
-                // -> public한 인스턴스변수들이라 바로 대입 시킨다.
-                //                Article article = new Article();
-                //                article.id = no;
-                //                article.title = title;
-                //                article.body = body;
-                // 3. 객체 생성후 데이터 초기화는 너무 귀찮으므로, 생성자를 정의하여
-                // 객체 생성시 다 넣도록 수정하자.
-                // ** ctrl + . 생성자 신공 **
-                // new Article(no, title, body);
-                Article article = new Article(no, title, body);
-                System.out.println("게시물이 저장되었습니다.");
-
-                // 4. 이제 데이터 묵음을 묶더라도 저장은 arraylist<Class>에 해줘
-                // -> 순서대로 저장된다.
-                // titles.add(title);
-                // bodies.add(body);
-                // numbers.add(no);
-                articles.add(article);
-                // 6. 이제 자료구조를 완전히 바꾼만큼. 빨간줄 뜨는 것들을 다 처리해준다.
-
-                no++;
+                addArticle();
                 continue;
             }
             if (command.equals("list")) {
-                list();
+                list(articles);
                 continue;
             }
             if (command.equals("update")) {
-                System.out.print("수정할 게시물 번호 : ");
-                int target = Integer.parseInt(scanner.nextLine());
-
-                int targetIndex = getIndexOfArticleNumber(target);
-
-                if (targetIndex == -1) {
-                    System.out.println("없는 게시물입니다.");
-                    continue;
-                }
-
-//                System.out.println("수정할 게시물 index : " + targetIndex);
-                System.out.print("제목 : ");
-                String title = scanner.nextLine();
-                System.out.print("내용 : ");
-                String body = scanner.nextLine();
-                //7. 수정도 마찬가지 articles의 index1개만 수정시켜주면 된다.
-                // -> 이 때, 수정된 값이 아니라 수정된 객체가 들어가야한다.
-                // -> **수정용 인스턴스를 또 하나 만들어야함**
-                // -> **수정이란? 기존 것은 덮어쓰기되서 삭제당함.**
-                Article article = new Article(targetIndex, title, body);
-                articles.set(targetIndex, article);
-//                titles.set(targetIndex, title);
-//                bodies.set(targetIndex, body);
-                System.out.println("수정이 완료되었습니다.");
-
-                list();
-
+                updateArticle();
                 continue;
             }
 
             if (command.equals("delete")) {
-                System.out.print("삭제할 게시물 번호 : ");
-                int target = Integer.parseInt(scanner.nextLine());
-                int targetIndex = getIndexOfArticleNumber(target);
-
-                if (targetIndex == -1) {
-                    System.out.println("없는 게시물 번호입니다.");
-                    continue;
-                }
-                //8.
-                articles.remove(targetIndex);
-//                titles.remove(targetIndex);
-//                bodies.remove(targetIndex);
-//                numbers.remove(targetIndex);
-                System.out.println("삭제가 완료되었습니다.");
-
-                list();
-
+                deleteArticle();
                 continue;
             }
+            //1. run()메소드가 리팩토링되었다면, 계속 리팩토링하도록 작성하자.
+            if (command.equals("search")) {
+                //2. 검색의 결과는 복수개가 나올수 잇으니 복수로 메소드명을 짓는다!!
+                // -> 무한반복 분기마다 early continue를 마지막에 달아주자! 예외만 맨 마지막에 남음
+                searchArticles();
+                continue;
+            }
+
 
             System.out.println("잘못 입력하였습니다.");
         }
     }
 
-    private int getIndexOfArticleNumber(int target) {
-        //9. 검색하는 데이터배열(이 바껴서)의 기준이 바뀜
-//        for (int i = 0; i < numbers.size(); i++) {
+
+    private void searchArticles() {
+        System.out.print("검색 키워드를 입력해주세요 : ");
+        String keyword = scanner.nextLine();
+
+        //7.
+        ArrayList searchedArticles = new ArrayList();
+
+        //2. 검색하려면, 반복문으로 articles(arraylist) 싹다 뒤져봐야한다.
         for (int i = 0; i < articles.size(); i++) {
-            // 10. **articles에서 get하면 article이 나오므로.. 내부데이터까지 한번 더 가야함**
-//            if (numbers.get(i) == target) {
+            if (articles.get(i).title.contains(keyword)) {
+                //3. 원하는 키워드를 포함했다면, 출력? 모아두기? -> 일단 출력해주자.
+                // -> 일부 list를 출력/조회하는 함수가 없기 때문이다.
+//                System.out.println("번호 : " + articles.get(i).id);
+//                System.out.println("제목 : " + articles.get(i).title);
+//                System.out.println("내용 : " + articles.get(i).body);
+//                System.out.println("====================================");
+                // -> test
+                //4. 이제 조회 list()함수를.. arraylist전체가 아닌...
+                // -> 여기서도 재활용할 수 있게 <파라미터로 Arraylist<Class>를 받아, 전체list가 아닌 일부list도 전체 다 출력되게> 리팩토링한다.
+
+                //6. 이제 일부 article list도 조회(출력)할 수 있게 파라미터를 심었으니,
+                // -> 검색된 article을 바로 출력 X -> 모아서 list로 건네주자.
+                searchedArticles.add(articles.get(i));
+            }
+        }
+        // 8. 검색된 결과를 list()조회->출력함수에 넘겨준다.
+        // -> 결과가 없어도 .size() 0처리되서 반복문이 안도니까 괜찮다.
+        list(searchedArticles);
+        //
+
+    }
+
+    private void deleteArticle() {
+        System.out.print("삭제할 게시물 번호 : ");
+        int target = Integer.parseInt(scanner.nextLine());
+        int targetIndex = getIndexOfArticleNumber(target);
+
+        if (targetIndex == -1) {
+            System.out.println("없는 게시물 번호입니다.");
+            return;
+        }
+        articles.remove(targetIndex);
+        System.out.println("삭제가 완료되었습니다.");
+
+        list(articles);
+    }
+
+    private void updateArticle() {
+        System.out.print("수정할 게시물 번호 : ");
+        int target = Integer.parseInt(scanner.nextLine());
+
+        int targetIndex = getIndexOfArticleNumber(target);
+
+        if (targetIndex == -1) {
+            System.out.println("없는 게시물입니다.");
+            return;
+        }
+
+        System.out.print("제목 : ");
+        String title = scanner.nextLine();
+        System.out.print("내용 : ");
+        String body = scanner.nextLine();
+        Article article = new Article(target, title, body);
+        articles.set(targetIndex, article);
+        System.out.println("수정이 완료되었습니다.");
+
+        list(articles);
+    }
+
+    private void addArticle() {
+        System.out.print("제목을 입력해주세요 : ");
+        String title = scanner.nextLine();
+        System.out.print("내용을 입력해주세요 : ");
+        String body = scanner.nextLine();
+        Article article = new Article(no, title, body);
+        System.out.println("게시물이 저장되었습니다.");
+
+        articles.add(article);
+        no++;
+    }
+
+    private void printHelp() {
+        System.out.println("add  : 게시물 등록");
+        System.out.println("list : 게시물 목록 조회");
+        System.out.println("update : 게시물 수정");
+        System.out.println("delete : 게시물 삭제");
+        System.out.println("search : 게시물 검색");
+    }
+
+    private int getIndexOfArticleNumber(int target) {
+        for (int i = 0; i < articles.size(); i++) {
             Article currentArticle = articles.get(i);
             if (currentArticle.id == target) {
                 return i;
@@ -142,13 +154,12 @@ public class Board {
         return -1;
     }
 
-    public void list() {
-        //11.
-        for (int i = 0; i < articles.size(); i++) {
-            Article article = articles.get(i);
-//            int number = numbers.get(i);
-//            String title = titles.get(i);
-//            String body = bodies.get(i);
+    //5. <내부에 반복문을 도는 조회함수>는 조회시 길이가변을 자동처리하도록 ArrayList 파라미터로 받게 한다.
+    // -> 전체 list(articles), 일부list(articles), 정렬된list, 1개, 0개 다 가능해진다.
+//    public void list() {
+    public void list(ArrayList<Article> list) {
+        for (int i = 0; i < list.size(); i++) {
+            Article article = list.get(i);
             System.out.println("번호 : " + article.id);
             System.out.println("제목 : " + article.title);
             System.out.println("내용 : " + article.body);
---
+++ src/main/java/board/test/Test.java
@@ -0,0 +1,37 @@
+package board.test;
+
+public class Test {
+    public static void main(String[] args) {
+        String txt1 = "가나다라" ;
+        String txt2 = "해당 내용은 테스트 입니다" ;
+        String txt3 = "가격은 29,000원 입니다" ;
+
+        // contains를 이용한 방법(true, false 반환)
+        if(txt2.contains("테스트"))
+            System.out.println("문자열 있음!");
+        else
+            System.out.println("문자열 없음!");
+
+//        // indexOf를 이용한 방법
+//        if(txt2.indexOf("테스트") > -1)
+//            System.out.println("문자열 있음!");
+//        else
+//            System.out.println("문자열 없음!");
+//
+//
+//        // matches를 이용한 방법
+//        if(txt2.matches(".*테스트.*"))
+//            System.out.println("문자열 있음!");
+//        else
+//            System.out.println("문자열 없음!");
+//
+//
+//        // matches를 이용하여 정규 표현식으로 문자열에 숫자가 있는지 확인
+//        if(txt3.matches(".*[0-9].*"))
+//            System.out.println("숫자 있음!");
+//        else
+//            System.out.println("숫자 없음!");
+
+
+    }
+}
