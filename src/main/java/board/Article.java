6d9fab8410df3a8fe7c30e2b9b281e29fd035300
is2js <tingstyle1@gmail.com>
2021-11-24 16:55:18 +0900

    요구사항8: 게시물 상세보기 구현 - 조회수는 올린뒤 보여주기

--- src/main/java/board/Article.java
+++ src/main/java/board/Article.java
@@ -4,14 +4,10 @@ public class Article {
     int id;
     String title;
     String body;
-    //1. 추가 정보데이터를 데이터용 클래스에 인변으로 준다.
-    // - 날짜도 일단 String으로 준다.
     String regDate;
     String writer;
     int hit;
 
-    //2. 각 객체 데이터 setter대신 생성자에서 초기화도 변수를 늘려준다.
-    // -> ide이용해서 다시 작성해준다.
     public Article(int id, String title, String body, String regDate, String writer, int hit) {
         this.id = id;
         this.title = title;
--- src/main/java/board/Board.java
+++ src/main/java/board/Board.java
@@ -50,14 +50,58 @@ public class Board {
                 searchArticles();
                 continue;
             }
+            //1. 새로운 명령어 대한 분기를 작성하고 함수를 작성한다.
+            if (command.equals("read")) {
+                readArticles();
+                continue;
+            }
+
 
             System.out.println("잘못 입력하였습니다.");
         }
     }
 
+    private void readArticles() {
+        // 2. 기본적으로 뿌려지는 것을 완성해보자.
+        //        상세보기할 게시물 번호를 입력해주세요 : 1
+        //                ==== 1번 게시물 ====
+        //        번호 : 1
+        //        제목 : 제목1
+        //                -------------------
+        //                내용 : 안녕하세요 ~
+        //                -------------------
+        //                        작성자 : 익명
+        //        등록날짜: 2021.03.07
+        //                ===================
+        System.out.print("상세보기할 게시물 번호를 입력해주세요 : ");
+        // 숫자를 받으니까 parseInt도 같이~
+        // 숫자 target(id) -> 메소드 -> arraylist 속 index 찾기 / 없으면 -1로 반환해서 early return(continue)
+        int target = Integer.parseInt(scanner.nextLine());
+        int targetIndex = getIndexOfArticleNumber(target);
+
+        if (targetIndex == -1) {
+            System.out.println("없는 게시물 번호입니다.");
+            return;
+        }
+        // id -> arraylist 속 index를 찾았다면, 꺼내야한다.
+        //3. index -1이 아니라서 return 안됬다면 상세보기됨 -> 조회수 1개 올려야한다!
+        // -> 조회수는 먼저 올리고 상세보기에서 올린 값을 보여줘야한다.
+        Article article = articles.get(targetIndex);
+        article.hit++;
+        System.out.printf("==== %d번 게시물 ====\n", article.id);
+        System.out.println("번호 : " + article.id);
+        System.out.println("제목 : " + article.title);
+        System.out.println("-------------------");
+        System.out.println("내용 : " + article.body);
+        System.out.println("-------------------");
+        System.out.println("작성자 : " + article.writer);
+        System.out.println("등록날짜 : " + article.regDate);
+        System.out.println("조회수 : " + article.hit);
+        System.out.println("===================");
+
+    }
+
     private void makeTestData() {
-        // 2. 조회수는 0으로, 날짜는 "2021.11.11"형식으로 일단 넣어준다.
-        // 13. 테스트데이터도, run()실행용 객체가 생성될 떄 생성자에서 추가되는데, 실행되는 날짜로서 추가해준다.
         articles.add(
                 new Article(1, "안녕하세요", "내용1입니다.", MyUtil.getCurrentDate("yyyy.MM.dd"), "익명", 0));
         articles.add(
@@ -110,8 +154,6 @@ public class Board {
         String title = scanner.nextLine();
         System.out.print("내용 : ");
         String body = scanner.nextLine();
-        // 3. 추가나, 수정시 필요한 데이터들도 일단은, 받기전에 테스트데이터를 넣어준다.
-        // "2021.11.11", "익명", 0
         Article article = new Article(target, title, body, "2021.11.11", "익명", 0);
         articles.set(targetIndex, article);
         System.out.println("수정이 완료되었습니다.");
@@ -125,7 +167,6 @@ public class Board {
         System.out.print("내용을 입력해주세요 : ");
         String body = scanner.nextLine();
 
-        // 12. add할 때, 테스트 데이터 -> 현재날짜를 문자열로 받아와서 추가해준다.
         String currentDate = MyUtil.getCurrentDate("yyyy.MM.dd");
 
         Article article = new Article(no, title, body, currentDate, "익명", 0);
@@ -153,16 +194,11 @@ public class Board {
         return -1;
     }
 
-    // 4. 출력이자 조회 함수에다가, 추가데이터를 출력하도록 추가해준다.
-    //    작성자 : 익명
-    //    등록날짜 : 2020.10.22
-    //    조회수 : 20
     public void list(ArrayList<Article> list) {
         for (int i = 0; i < list.size(); i++) {
             Article article = list.get(i);
             System.out.println("번호 : " + article.id);
             System.out.println("제목 : " + article.title);
-            //            System.out.println("내용 : " + article.body);
             System.out.println("작성자 : " + article.writer);
             System.out.println("등록날짜 : " + article.regDate);
             System.out.println("조회수 : " + article.hit);
